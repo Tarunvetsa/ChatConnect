5b47a0e0e144066c2301c0de67c425d7926e7950
Tarunvetsa <vetsatarun@iitbhilai.ac.in>
2024-02-06 00:13:38 +0530

    Messages for joining conversation and Participants to the room

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic,Message
 from .forms import RoomForm
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
@@ -76,7 +76,18 @@ def home(request):
     
 def room(request,pk):
     room=Room.objects.get(id=pk)
-    context={'room':room}
+    room_messages=room.message_set.all().order_by('-created')
+    participants=room.participants.all()
+    
+    if request.method=="POST":
+        message= Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room',pk=room.id)
+    context={'room':room,'room_messages':room_messages,'participants':participants}
     return render(request,'base/room.html',context)
 
 
