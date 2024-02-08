20b27069de517eaa42fbee5d6ab77ee243b68aa4
Tarunvetsa <vetsatarun@iitbhilai.ac.in>
2024-02-08 17:23:22 +0530

    Customised User model, Added Avatar for a profile

--- chatconnect/settings.py
+++ chatconnect/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     
 ]
 
+AUTH_USER_MODEL='base.User'
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
@@ -119,10 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL ='/images/'
 
 STATICFILES_DIRS =[
     BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT = BASE_DIR/'static/images'
 
 # STATIC_ROOT
 # Default primary key field type
